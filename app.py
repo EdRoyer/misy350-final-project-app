039a905f827a95531a98ee9e4d26777ee1d01d59
Your Name <your.email@example.com>
2026-04-01 01:50:18 -0400

    Finished sidebar setup and some progress on booking appointments

--- app.py
+++ app.py
@@ -5,11 +5,21 @@ from datetime import datetime
 import uuid
 import time
 
-st.set_page_config("Doctor's Appointments", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config("Doctor's Appointments", page_icon=":gem:" , layout="wide", initial_sidebar_state="expanded")
 
+appointments = []
+
+json_path_appointments = Path("appointments.json")
+
+if json_path_appointments.exists():
+    with open(json_path_appointments, "r") as f:
+        requests = json.load(f)
+
+#Establishes Login/Registration as default page
 if "page" not in st.session_state:
     st.session_state["page"] = "Login/Registration"
 
+#Sets up sidebar framework for pages
 with st.sidebar:
     if st.button("Login/Registration", key="log/reg_btn", type="primary", use_container_width=True):
         st.session_state["page"] = "Login/Registration"
@@ -19,5 +29,49 @@ with st.sidebar:
         st.session_state["page"] = "Book_Appointment"
         st.rerun()
 
+    if st.button("Appointment Dashboard", key="Appointment_Dashboard_btn", type="primary", use_container_width=True):
+        st.session_state["page"] = "Appointment_Dashboard"
+        st.rerun()
+
+    if st.button("Reschedule Appointments", key="Reschedule_Appointment_btn", type="primary", use_container_width=True):
+        st.session_state["page"] = "Reschedule_Appointments"
+        st.rerun()
+
+    if st.button("Delete Appointments", key="Delete_Appointment_btn", type="primary", use_container_width=True):
+        st.session_state["page"] = "Delete_Appointments"
+        st.rerun()
+
+
+st.markdown("# Patient Appointment Tracker")
+
+#Login/Registration:
+#I'm thinking u can represent this as two tabs within the page, with login as the default, but it's up to you.
+
+
 
 #CREATE: Book Appointments
+if st.session_state['page'] == "Book_Appointment":
+    st.header("Book Appointment")
+
+    appointment_time = st.date_input("Appointment time")
+    symptoms = st.text_input("Enter Symptoms", key="symptoms")
+
+    book_now_btn = st.button("Book Now", key="book_now_btn", use_container_width=True)
+
+    #if book_now_btn:
+        
+
+
+
+
+
+#READ: Appointment Dashboard
+
+
+#UPDATE: Reschedule Appointments
+if st.session_state['page'] == "Reschedule_Appointments":
+    st.header("Reschedule Appointments")
+
+#DELETE: Delete Appoinments
+if st.session_state['page'] == "Delete_Appointments":
+    st.header("Delete Appointments")
